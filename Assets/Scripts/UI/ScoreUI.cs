4d72ffb3e95302c6731d4318423ac2411f947e1f
Sorawis Leepiyasakulchai <sorawisson@gmail.com>
2026-08-24 21:47:53 +0700

    feat: add score UI
    Added UI to display the current score and session high score during gameplay.

---
+++ Assets/Scripts/UI/ScoreUI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreUI : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private ScoreManager scoreManager;
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private TMP_Text highScoreText;
+
+    private void Update()
+    {
+        scoreText.text = $"SCORE: {scoreManager.CurrentScore:0000}";
+        highScoreText.text = $"HIGH SCORE: {scoreManager.HighScore:0000}";
+    }
+}
